93cd76f06e88766bb2426bbfb76b02bb6e5c979c
ahmedskamar <meandmyswordareinvincible>
2019-07-25 11:37:59 +0200

    Add layout phase 1

--- app/src/main/java/com/example/quraanuploader/view_containers/MainActivity.kt
+++ app/src/main/java/com/example/quraanuploader/view_containers/MainActivity.kt
@@ -1,12 +1,19 @@
-package com.example.quraanuploader
+package com.example.quraanuploader.view_containers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quraanuploader.R
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        openFragment()
     }
+
+    private fun openFragment() {
+//       findNavController(navHost).navigate(R.id.action_global_mediaFragment)
+        }
 }
---
+++ app/src/main/java/com/example/quraanuploader/view_containers/MediaFragment.kt
@@ -0,0 +1,24 @@
+package com.example.quraanuploader.view_containers
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.quraanuploader.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MediaFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_media, container, false)
+    }
+
+}
